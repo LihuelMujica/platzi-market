dfa2b834ae770a464828499e2c46226eca526d71
LihuelMujica <lihuelmujica@gmail.com>
2022-05-30 18:12:53 -0300

    Agrego muchas cosas

---
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.platzi.market.web.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
